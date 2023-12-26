2b100a2405410669c4d3ca448d8b1a7841ebb6f7
Arad <arad.koluok@post.runi.ac.il>
2023-12-26 20:48:39 +0200

    damkaboard

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,15 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int boardLength = Integer.parseInt(args[0]);
+		for (int i = 0; i < boardLength; i++) {
+			for (int j = 0; j < boardLength; j++) {
+				if ( i % 2 == 1 ) {
+					System.out.print(" ");
+				}
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 	}
 }
