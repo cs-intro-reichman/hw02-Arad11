f41cde298295b43ffb239974631e6d26152a57b8
Arad <arad.koluok@post.runi.ac.il>
2023-12-26 16:25:54 +0200

    Divisor and InOrder is ready

--- InOrder.java
+++ InOrder.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+
 /**
  *  Generates and prints random integers in the range [0,10),
  *  as long as they form a non-decreasing sequence.
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		Random rnd = new Random();
+		int someNumber = -1;
+		int nextNumber = rnd.nextInt(0,10);
+		while(someNumber <= nextNumber) {
+			System.out.print(nextNumber + " ");
+			someNumber = nextNumber;
+			nextNumber = rnd.nextInt(0,10);
+		}
 	}
 }
