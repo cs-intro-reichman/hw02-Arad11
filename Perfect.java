b2029e96312a7ee3358bc23b6b7e04f55610f145
Arad <arad.koluok@post.runi.ac.il>
2023-12-26 20:36:15 +0200

    Perfect works

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,22 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int sum = 0;
+		String divisors = " = ";
+		int number = Integer.parseInt(args[0]);
+		for(int i = 1; i < number; i++) {
+			if ((number % i) == 0 ) {
+				 sum += i;
+				 divisors += i + " + ";
+			}
+		}
+
+		if (sum == number) {
+			 divisors = divisors.substring(0,divisors.length() - 2);
+			 System.out.println(number + " is a perfect number since " + number + divisors);
+		}
+		else {
+			System.out.println(number + " is not a perfect number");
+		}
 	}
 }
