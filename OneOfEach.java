9c1ebab978f009b7983b9b9fa061c1bc1790ae6e
Arad <arad.koluok@post.runi.ac.il>
2023-12-28 08:50:28 +0200

    Delete unnecessarry space

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,30 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		int someOfBoys = 0;
+		int someOfGirls = 0;
+		String childrenOrder = "";
+
+		if(Math.random() < 0.5) {
+			someOfBoys++;
+			childrenOrder += "b";
+		}
+		else {
+			someOfGirls++;
+			childrenOrder += "g";
+		}
+
+		while(someOfBoys == 0 || someOfGirls == 0) {
+			if(Math.random() < 0.5) {
+				someOfBoys++;
+				childrenOrder += " b";
+			}
+			else {
+				someOfGirls++;
+				childrenOrder += " g";
+			}
+		}
+		System.out.println(childrenOrder);
+		System.out.println("You made it... and you now have " + (someOfBoys + someOfGirls) + " children.");
 	}
 }
