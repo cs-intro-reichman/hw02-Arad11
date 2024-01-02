a38c3bd7fb7cb52a482d957653d4ae5222fee843
Arad Koluok <arad.koluok@runi.ac.il>
2024-01-02 23:11:32 +0200

    non

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -20,15 +20,6 @@ public class OneOfEachStats {
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
 		
-		//// In the previous version of this program, you used a statement like:
-		//// double rnd = Math.random();
-		//// Where "rnd" is the variable that stores the generated random value.
-		//// In this version of the program, replace this statement with:
-		//// double rnd = generator.nextDouble();
-		//// This statement will generate a random value in the range [0,1),
-		//// just like you had in the previous version, except that the 
-		//// randomization will be based on the given seed.
-		//// This is the only change that you have to do in the program.
 		for (int i = 0; i < T; i++) {	
 			if(generator.nextDouble() < 0.5) {
 				boysInFamily++;
